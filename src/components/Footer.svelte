8951ab1c0337271efb2f46d5968917f0228448cf
yenn01 <yenn.m17@gmail.com>
2022-12-23 04:56:30 +0800

    Added data collection disclosure and disclaimer

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -4,6 +4,10 @@
 
 <footer>
 	<small>&copy; shorter 2022</small>
+    <br>
+    <small>User's region of origin is recorded during redirection and is used purely for research purposes.</small>
+    <br>
+    <small>Website is still in development. Use at own risk.</small>
 </footer>
 
 <style>
