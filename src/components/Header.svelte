906f926e00d4319085216b31f81532513f39b682
yenn01 <yenn.m17@gmail.com>
2022-12-22 21:40:58 +0800

    Replaced temp logo with logo

--- src/components/Header.svelte
+++ src/components/Header.svelte
@@ -1,20 +1,26 @@
 <script>
     import Input from './Input.svelte';
+    let src = "./logo.png"
 
 </script>
     <div class="cont-header"> 
-        <div>shorter</div>
+        <img {src}>
        
     </div>
 
 <style>
     .cont-header {
         display: flex;
-        padding:0.2rem;
+        padding:0.5rem;
+        padding-top: 1rem;
         justify-content: space-between;
         margin-bottom: 1rem;
         height: 3rem;
         align-items: center;
+    }
+
+    img {
+        width:15rem;
     }
 
 </style>
